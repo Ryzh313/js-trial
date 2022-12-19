7c2d101771700eaf13be8b0e7885a315404063a4
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2022-12-19 02:11:26 +0200

    sum-of-number

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-let a = 'ghjkl';
+function calculateTotal(number) {
+    let sum = 0;
+    for (let i = 0; i <= number; i += 1)
+    sum += i;    
+    return sum;
+}
+console.log(calculateTotal(24));
